96272d15d10e3bfd90f0c4ab36b842443ff62154
j__h.106 <84125506+vmffyflf@users.noreply.github.com>
2022-05-14 17:04:34 +0900

    assingment

--- src/page/assignment/Gomem.js
+++ src/page/assignment/Gomem.js
@@ -3,96 +3,161 @@ import React from 'react';
 function Gomem() {
     return (
         <div className={{justifyContent: 'flex', alignItems: 'flex'}}>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/f5a99244d27521377c6c2464dd2987244918f855ac98c5e90e91110df55ea8d3806af9d7382cde420cd842e00c8214a61a02750b72aed3d808dcd3c3898cdd01edeea72acad4adf7d70b69a178ef05821f8389f407906bcb433bcd074cc69f833339ffef90ffaa7e228db9d87297a031"}
                     style={{width: '300px', height: '300px'}}/>
-                <b style={{display: 'inline-block', padding: '0px',verticalAlign: 'top'}}>도파민박사</b>
-                <p style={{display: 'inline-block', padding: '0px',verticalAlign:'top'}}>도파민 박사는 박사 컨샙의 고정멤버이다.</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>도파민박사</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>도파민 박사는 박사 컨샙의 고정멤버이다. 자칭 대한민국 최고의 박사. 모델의 특징으로는 머리가
+                        벗겨진 아인슈타인을 닮은 모델을 사용한다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/0490fbc00115c76fd1239edbecd8eedd0cb636d851205b2678678d19f007e3c563b69358dc87470885710c5a44ca60266e89a57d2bd1f5fe5867b3be227fd49a17b9c140aaf357b6fc91417d7aa6798d4aa441143a34d76ebb8ff48d40a0e5e5c950dc33acee0c2f1ee1e0ebf43532ae"}
                     style={{width: '200px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>독고혜지</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>독고혜지</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>한국 일진 여고생 틱톡커의 컨샙으로 틱톡커들이 많이 입는 타이트한 교복에 진한 화장과 검은
+                        마스크가 특징이다. 성격이 개방적이라서 여러 고멤들과 친해져서 합방에서도 활약하는 모습을 보여준다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/824c70c32cbe730ad5084253598539f0fe04dc542e2bf432215387d0b742cbbc3ded41c810513f1f10dfc914cf6deea7decace4165f38c188b99e5befe36011ba731a22fe03383a9171af2eb8e7c5cac40bcd70f21ed882a40bd05490946211bc0b54c93f929debed49adbe500ff22ed"}
                     style={{width: '200px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>륀트게늄</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>뢴트게늄</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>캐릭터는 조커이지만 내추럴(노컨셉)로 지원했다. 기본 캐릭터성이 유니크하여 여러 방면에서 놀라운
+                        모습을 보여준다. 자신이 씹덕임을 당당히 밝히고 이세계 아이돌의 메니저 역할을 하고있다. 원래는 성우지망생으로 목소리가 생각보다 좋다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/bff65fff6859cb7ecc14091298b83a8cde320f2c73189762285db31a07f13c999029dd474b066aac7ca49b171b6112904ff5810018cb5611678b692df380b3d20751762e1154176fbeca7d4c82eb770734ddae8fee835dbe4f5a67a0113fd92bac8166f8563f3acf883ab4ff8949fb06"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>미츠네 하쿠</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>미츠네 하쿠</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>로봇형 캐릭터를 컨샙으로 하츠네 미쿠가 모티브로 했다. 마스터는 새우튀김이며, 왁물원에 글을 쓸
+                        때 오류가 난것처럼 특수문자를 마구 섞어 쓴다. 자율학습기능과 노래기능을 갖췄다. 로봇이라 성별은 딱히 없지만 귀여운 외형, 목소리, 노래 실력으로 인기가 아주
+                        좋다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/cb72a5499e3617bc73a72a730a0f9224c9170555b86eb537bb575468736e7202cfb4e5815394a90692e519e546857965ed614b5acd9788e70ff95a58083ca585956564b328430cf81f071a6680991eece9e10705deb167f78668da25b7d805df2275e4ce8908a4422eec4e93604b9489"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>부정형인간</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>부정형인간</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>컨샙은 이름 그대로의 컨샙이며, 자신감과 자존감 제로. 스스로에 대해 매우 부정적인 태도를 갖고
+                        있으며, 피해의식에 때때로 공격적이다. 영화를 굉장히 좋아하며 영화 평론의 논리적인 질문을 하며 영화에 대해서는 열정적인 면모를 보여준다. </p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/f323e261b0ce6d90bac9b328eabba944e85df8f8eef72baa0c119585ad79711008e2ee6bae915ba442e38d2e18deb34d64a73e54fd819dab0ebe23a281da1ac9de01b4b01f581aa44e67342c79b7cb2b5e60ce332a622b6d9433b716dac133b2aa6c005557e5227d5456ec288e04bec9"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>비밀소녀</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>비밀소녀</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>천사소녀 네티 컨샙으로 마법소녀라 그런지 우왁굳이 이름을 마법소녀로 혼동하는 경우가 있다.
+                        본인 피셜 천사소녀 그 자체이며 셜록스와는 아직 쫓고 쫓기는 사이라고 한다. 약칭은 비소지만 소녀라는 닉네임과 아바타와는 달리 말투와 억양에서
+                    연식이 많은 듯한 느낌이 강해서 비밀이모라고 불린다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>>
                 <img
                     src={"https://w.namu.la/s/bd022f244ff2a70f606a9908643491aeec02291cf285a31f938c432cc3b20c64dac393c0f90bd6780ce839e4b0ecf67d8b6297a32be3070ca5d62b6187d7295c3734eea875471871fef42724bc3808ebbdedf0643bbc597dc16ad60b5c5b40156082182703576368b4163911ec8b54a4"}
                     style={{width: '350px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>비즈니스 킴</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>비즈니스 킴</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>신성 로마 제국의 귀족 컨샙의 캐릭터다. 파이널 판타지의 세피로스 아바타를 사용하고 있으며 아버지의 회사를 물려받기 싫기도 하고 왁타버스에
+                        참가하고 싶기도 해서 한국으로 귀화했다. 면접 당시 등장부터 면접장이 천박하다고 하며 귀족스러움을 강조했다.
+                        정작 귀족 컨셉인데도 평소 모습은 전혀 귀족 같지 않고 연기를 못하는 게 특징. 노래도 못하는 편이다.
+                        고정멤버중에 여성 멤버한테는 레이디라고 부르며 종종 기사도 혹은 신사 컨샙을 가지려고 한다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50',display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/f0bf075b96e907eaa3f4e9aa5f7cf99bd5bf8207aea144b703a61cedf5d1a4bed4feb7573fa263c60065ca6aafd9efd36875a2637e5e46ef6e66b4c24057c331108e12782aba2451f0b56148703b2e5ee8db668ebf7fd2b0bc637b11ade7ebd6c5fcef258000b3b58388dbd04d0928b3"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>새우튀김</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>새우튀김</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>내추럴(노컨셉)로 지원했다. 도파민 박사 처럼 과학자 모델을 사용하며 이를 여겨본 도파민 박사가
+                    조수로 채용했다. 도파민 박사에 의해 역할이 생겼지만 기본적으로 내추럴이라서 다소 어색함이 느껴진다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/bae1032a9ccc23839820b70ba1da6f32705a489bf1ab26e4d09d2059abd232c8780d5b86803136a85e026fff944b9e9ce589771d2912fbb583f2a5784a2f5624b87cc1fe304989003696607cc0c0436299e866d545e7a226265f4539397646c156914639227c7adb011fb10b6f4bc012"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>소피아</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>소피아</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>컨샙을 들고 오긴 했지만 본체가 너무 티나서 마미손 같은 포지션으로 뽑혔다. 캐릭터 컨샙은 선량한
+                    시민인척하는 도적이다. 평범하게 이거저거 주워다가 파는 일을 한다고 한다. 해외 수출입도 가끔 한다고 한다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50',display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/8645ca2f85391cd1d1cb0722ae92c21abf8593ae29a03d00e3ecc6f4da9f7ff302ae3b4e42bccbeb40b3c3aa26114d0675d5c38c7aa81ad45a2e3f14d98136962e11b7ea89ed026713e8bba28969c6e59468eeb78101201970d909f76dc830bdfb0418e741ae38967d1dfa15f2cc131f"}
                     style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>이덕수 할아바이</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>이덕수 할아바이</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>고독한 노인 컨샙의 캐릭터이며 미학 강의를 할때 고독한 컨샙때문인지 조용히 강의를 듣고 공부하는
+                    우등생의 면모를 보여준다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left',display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/e5ef7976dca7f9e941c68dd00f1f518b704eedd35a11ba34b2bf19bd56ee3f84f2cde94a819fc7f711e88987c323c415c915f8d45cb15adddd6de29ac2232fe54bd6ad06d14db4509cee45d142bc8dd3f06f9b8c0221e8a1b7d57c1da001ffcaa2f9e69176f400e95ee5d51eda69cd59"}
                     style={{width: '200px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>풍신</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>풍신</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>늙은 마법사의 컨샙으로 바람마술을 사용하는 매직키드 마수리의 26대 독자라고 한다. 마법을 쓸때 바람 풍을
+                    외친다. 서울에 있는 숲에 산다고 하며 나이는 600살이 넘었으며 모태솔로라고 한다. 과학을 싫어하며 흔한 노인 컨샙이랑 다르게 마법사, 신선같은 컨샙을 밀며 독보적인
+                     아이덴티티로 합격했다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/ceb8da561ce6505a7b9574360f158b4ea3be1fba2da0b951ab914e1d442013c8a9e4644fb12fb20bee69fc9a7ad9864ed07cc295ca16014469bf382b2d4a05f2449d24bedfe6c998d66c233944b6e901f691cd1e4fef029578999f6fcc617164611a66f1ad8e9aae693d275a227ee4a2"}
                     style={{width: '200px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>해루석</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>해루석</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>내추럴(노컨샙)으로 지원했지만 컨샙같은 내추럴이라 독보적인 캐릭터성으로 이미 인기가 있었기 때문에
+                    합격됬다. 성우 못지 않은 목소리를 가져서 고멤 합방 영상의 더빙을 종종하곤 한다. 캐릭터로 잘생겨서 카사노바의 포지션을 하고있다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/90f7a96175bc49ef42b84438c9fc3b5db24205178cdd947441d9f3694c6a3b1531c3b1f08c5e79060c93362d186c1c4568fcd537eb29b96819a7b694ecb1eca9312c42725fd257e1ba63f8ca11118c3a1aa7abac0832063dde05a7de489b1532b5b55bdd421f31fc960c76fe9207f594"}
                     style={{width: '300px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>히키킹</p>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>히키킹</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>졸렬잎 마을에서 온 우치하 일족 외국인으로 ~스무니다체를 사용하며 목소리 톤이 높은 편이다. 뒤죽박죽한 억양과
+                    ,가끔 나오는 일본어가 특징이다. 우치하 귀살대에 소속되어 있기에 귀멸의 칼날에 등장하는 젠이츠의 옷을 입고 벽력일섬 같은 능력을 구사한다. 졸렬잎 마을 출신임에도 불구하고
+                        귀살대 소속인 이유는 나루토는 이제 한물갔고 요즘은 귀멸의 칼날이 대세이기 때문이라고한다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'right', marginRight: '50'}}>
+            <div style={{width: '50%', height: '400px', float: 'right', marginRight: '50', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/24d4c416174dabab6f33f3af1c3e1eca7ea4d674a2081d0913485093e209eafd39a35030afb7deb600f372d2972cae7467f7a236009a0c0334cbf17c4264dab0d9a6b3eecc9e1c7eedeaf61399b4f2b5ea8225ccf94a17b3a335540007b4b5d0fb6111f64859c95d246c37e17c039263"}
-                    style={{width: '200px', height: '300px'}}/>
-                <p style={{display: 'flex'}}>카르나르 융터르</p>
+                    style={{width: '230px', height: '300px'}}/>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>카르나르 융터르</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>심리상담사의 컨샙으로 학위가 없어서 영업정지 당한 심리상담사이다. 분석심리학의 무의식, 초자아 드립을 친다.
+                    무의식 빼고 아는게 없다 그래서 심리학자라고 안하고 심리상담사라고 한다.</p>
+                </div>
             </div>
-            <div style={{width: '50%', height: '500px', float: 'left'}}>
+            <div style={{width: '50%', height: '400px', float: 'left', display: 'flex'}}>
                 <img
                     src={"https://w.namu.la/s/d1631c5b96d9ecb0c936f84788e61afb6ec70a6f4b7d0a80a0bc3baf12ed2cad99098e3a89d3d2219c1ed6341605c086ba95fbee994ed8f82882feb4b3b4cde4949c58d5563a8d5071cf0fbefd3f8c5aa0059e911713429e376a40055fd96e5808a9649cc5c109c66722e52985cf9eb2"}
-                    style={{width: '250px', height: '300px'}}/>
-                <p style={{display: 'flex', padding: '0px'}}>프리터</p>
+                    style={{height: '300px'}}/>
+                <div style={{}}>
+                    <b style={{padding: '0px', fontSize: '20px'}}>프리터</b><br/>
+                    <p style={{marginTop: '10px', padding: '0px'}}>풍성한 머리카락에 정장을 입은 프리저이다. 프리터라는 이름에 맞게 평소에 알바를 하며 지냈는데, 얼마전에 짤려서
+                    요즘은 일일알바를 한다고 한다.</p>
+                </div>
             </div>
 
 
